62e53acd1bfdff626bebde76c6315be03bfbb24b
AlexZargarian <92695198+AlexZargarian@users.noreply.github.com>
2022-04-30 21:41:39 +0400

    Add files via upload

---
+++ Spring.java
@@ -0,0 +1,32 @@
+public class Spring {
+    private double k;
+
+
+    public Spring(){
+
+    }
+
+    public double getK() {
+        return k;
+    }
+
+    public void setK(double k) {
+        this.k = k;
+    }
+
+    static double move(double t, double dt, double x0, double v0){
+        // i dont know what it should return
+
+    }
+    static double move(double t, double dt, double x0){
+        // i dont know what it should return
+    }
+    static double move(double t0, double t1, double dt, double x0, double v0){
+        // i dont know what it should return
+    }
+    static double move(double t0, double t1, double dt, double x0, double v0, double m){
+        // i dont know what it should return
+    }
+
+}
+
