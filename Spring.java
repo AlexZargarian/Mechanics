e74296acfcff599b87fb0e1027babb22db1b163d
AlexZargarian <92695198+AlexZargarian@users.noreply.github.com>
2022-05-07 09:02:28 +0400

    Update Spring.java

--- Spring.java
+++ Spring.java
@@ -1,32 +1,62 @@
 public class Spring {
     private double k;
 
-
     public Spring(){
-
+        this.k=1;
     }
 
-    public double getK() {
-        return k;
-    }
-
-    public void setK(double k) {
-        this.k = k;
-    }
-
-    static double move(double t, double dt, double x0, double v0){
-        // i dont know what it should return
+    public Spring(double k){
+        this.k = 1;
 
     }
-    static double move(double t, double dt, double x0){
-        // i dont know what it should return
+    public double getK(){
+        return this.k;
     }
-    static double move(double t0, double t1, double dt, double x0, double v0){
-        // i dont know what it should return
-    }
-    static double move(double t0, double t1, double dt, double x0, double v0, double m){
-        // i dont know what it should return
+    private void setk(double k){
+        this.k=k;
     }
 
+    public double[] move(double t, double dt, double x0, double v0){
+        int n= (int) ((t)/dt);
+        double w=Math.sqrt(k);
+
+        double[] result= new double[n];
+
+        for (int i = 0; i < n; i++){
+            result[i]=(x0*Math.cos(w*(t)+(v0/w)*Math.sin(w*(t))));
+        }
+
+        return result;
+    }
+    public double[] move(double t, double dt, double x0){
+        int n= (int) ((t)/dt);
+        double w=Math.sqrt(k);
+
+        double[] result= new double[n];
+
+        for (int i = 0; i < n; i++){
+            result[i]=(x0*Math.cos(t));
+        }
+
+        return result;
+    }
+    public double[] move (double t0, double t1, double dt, double x0, double v0){
+        return move(t0,t1,dt,x0,v0,1.0);
+    }
+    public double[] move(double t0, double t1, double dt, double x0, double v0, double m){
+        int n= (int) ((t1-t0)/dt);
+        double w=Math.sqrt(k/m);
+
+        double[] result= new double[n];
+
+        for (int i = 0; i < n; i++){
+            result[i]=(x0*Math.cos(w*(t0+i*dt)+(v0/w)*Math.sin(w*(t0+i*dt));
+        }
+
+        return result;
+    }
+public Spring inParralel(Spring that){
+       
 }
 
+}
